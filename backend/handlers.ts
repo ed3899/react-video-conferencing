5cdcbfbb1e54a101b87001c0579d43d195c0c891
Eduardo Casanova <ed.wacc1995@gmail.com>
2023-05-27 19:13:22 +0000

    added query presence checking and regex checking for room name

--- backend/handlers.ts
+++ backend/handlers.ts
@@ -1,43 +1,69 @@
-import {Request, RequestHandler} from "express";
+import {Request, RequestHandler, Response} from "express";
 import createToken from "./token";
+import R from "ramda";
+import {VideoGrant} from "livekit-server-sdk";
 
 type TokenResponse = {
-  token?: string;
+  token: string;
   errors: string[];
 };
 
-const check = (req: Request) => {
-  const {roomName, identity, name, metadata} = req.query;
-  let jres: TokenResponse = {
-    errors: [],
-  };
+type QueryParams = {
+  roomName: string;
+  identity: string;
+  name: string;
+  metadata: string;
+};
 
-  if (typeof identity != "string" || typeof roomName != "string") {
-    jres.errors?.push(
-      "the query params 'identity' and 'roomName' must be strings"
+/**
+ * @abstract Returns an array with formatted errors
+ * @param req
+ * @returns
+ */
+const queryPresenceCheck = (req: Request): string[] => {
+  const expectedKeys: (keyof QueryParams)[] = [
+    "roomName",
+    "identity",
+    "name",
+    "metadata",
+  ];
+  const presentKeys = R.keys(req.query);
+
+  if (presentKeys.length != expectedKeys.length) {
+    // Return an array with formatted errors
+    return R.map(
+      (q: string | number) => `the query param '${q}' must be present`,
+      R.difference(expectedKeys, presentKeys)
     );
   }
 
-  if (Array.isArray(name)) {
-    jres.errors?.push("provide max one name");
-  }
-
-  if (Array.isArray(metadata)) {
-    jres.errors?.push("provide max one metadata string");
-  }
-
-  return jres;
+  return [];
 };
 
-const getTokenHandler: RequestHandler = (req, res) => {
-  const {roomName, identity, name, metadata} = req.query;
-  const tr = check(req);
-
-  if (tr.errors.length > 0) {
-    res.status(403).json(tr).end();
+export const getTokenHandler: RequestHandler<
+  {},
+  Partial<TokenResponse>,
+  any,
+  QueryParams
+> = (req, res) => {
+  const errors = queryPresenceCheck(req as any);
+  if (errors.length > 0) {
+    res.status(403).json({
+      errors,
+    });
     return;
   }
 
-  res.sendStatus(403).json(createToken()).end();
-  return;
+  const roomName = req.query.roomName;
+  const roomPattern = /\w{4}\-\w{4}/;
+  const matches = R.match(roomPattern, roomName);
+
+  if (R.isEmpty(matches)) {
+    res.status(403).json({
+      errors: [
+        `The room name '${roomName}' you provided must be in the following format: wwww-wwww`,
+      ],
+    });
+    return;
+  }
 };
--- backend/index.ts
+++ backend/index.ts
@@ -1,44 +1,11 @@
 import config from "config";
 import express from "express";
-import createToken from "./token";
-
-const roomPattern = /\w{4}\-\w{4}/;
+import { getTokenHandler } from "./handlers";
 
 const app = express();
 const port = config.get("express.port");
 
-type TokenResponse = {
-  token: string;
-  error: string;
-};
-
-app.get("/getToken", (req, res) => {
-  const {roomName, identity, name, metadata} = req.query;
-  let jres: Partial<TokenResponse>;
-
-  if (typeof identity != "string" || typeof roomName != "string") {
-    jres = {
-      error: "the query params 'identity' and 'roomName' must be strings",
-    };
-    res.status(403).json(jres).end();
-    return;
-  }
-
-  if (Array.isArray(name)) {
-    jres = {
-      error: "provide max one name",
-    };
-  }
-
-  if (Array.isArray(metadata)) {
-    jres = {
-      error: "provide max one metadata string",
-    };
-  }
-
-  res.send(createToken()).end();
-  return;
-});
+app.get("/getToken", getTokenHandler);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
